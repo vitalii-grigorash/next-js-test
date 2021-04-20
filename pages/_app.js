3c041c8e7ee2ff00f1881a31365777b7981a7589
vitalii-grigorash <vitalii.grigorash@yandex.ru>
2021-04-20 14:26:28 +0300

    added server.js for deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/styles.css';
+import '../styles/globals.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Head from 'next/head';
