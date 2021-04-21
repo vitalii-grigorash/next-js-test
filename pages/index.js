f10c5fe99d27eb42f8cd9468d47c598a9dc77f0e
vitalii-grigorash <vitalii.grigorash@yandex.ru>
2021-04-21 11:26:23 +0300

    test

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import Head from 'next/head';
-
 export default function MainPage () {
 
   return (
 
     <div className='main'>
-
-      <Head>
-        <title>Main Page</title>
-      </Head>
 
       <h1 className="main__title">Main Page</h1>
       
