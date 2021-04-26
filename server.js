0b5accccbcaf9b4e0aab00b9dd606722b568458b
vitalii-grigorash <vitalii.grigorash@yandex.ru>
2021-04-26 12:31:14 +0300

    test

---
+++ server.js
@@ -0,0 +1,25 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const { PORT = 3000 } = process.env;
+const app = next({ PORT })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+
+    if (pathname === '/a') {
+      app.render(req, res, '/a', query)
+    } else if (pathname === '/b') {
+      app.render(req, res, '/b', query)
+    } else {
+      handle(req, res, parsedUrl)
+    }
+  }).listen(3000, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+})
