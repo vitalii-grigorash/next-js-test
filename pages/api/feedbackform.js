cb61413fae0b02f835ad952e27207569c4db03a8
vitalii-grigorash <vitalii.grigorash@yandex.ru>
2021-05-19 16:27:39 +0300

    test

--- pages/api/feedbackform.js
+++ pages/api/feedbackform.js
@@ -1,22 +1,26 @@
+import { nodemailerConfig } from '../../nodemailerConfig';
+
 export default (req, res) => {
+
     const nodemailer = require('nodemailer');
     const data = req.body;
+
     const smtpTransport = nodemailer.createTransport({
-        service: 'Gmail',
-        port: 465,
+        service: nodemailerConfig.service,
+        port: nodemailerConfig.port,
         auth: {
-            user: 'vitalii.grigorash.job@gmail.com',
-            pass: '1q2w3e4R5Tgrigorash',
+            user: nodemailerConfig.senderMail,
+            pass: nodemailerConfig.senderPassword,
         }
-    });
+    })
 
     const mailOptions = {
-        from: 'vitalii.grigorash.job@gmail.com',
-        to: 'vitalii.grigorash@yandex.ru',
+        from: nodemailerConfig.senderMail,
+        to: nodemailerConfig.recipientMail,
         subject: `Сообщение от ${data.name}`,
         html: `
             <h3>Форма обратной связи с сайта:</h3>
-            <h3>stroi.ooorobip.beget.tech</h3>
+            <h3>${nodemailerConfig.website}</h3>
             <ul>
                 <li style="margin-bottom: 10px"><span style="font-weight:bold">Имя:</span> ${data.name}</li>
                 <li style="margin-bottom: 10px"><span style="font-weight:bold">Номер телефона:</span> ${data.number}</li>
